9e6cb24fd1e69e36f783c29f0f9fc5683bc96c8f
Hauke Fischer <Haukee.fischer@gmail.com>
2023-06-29 15:31:28 +0200

    Improved graphics and brick class is useable without collisions

--- bricks.py
+++ bricks.py
@@ -1,48 +1,14 @@
 from turtle import Turtle
-import random
- 
-COLOR_LIST = ['light blue', 'royal blue',
-              'light steel blue', 'steel blue',
-              'light cyan', 'light sky blue',
-              'violet', 'salmon', 'tomato',
-              'sandy brown', 'purple', 'deep pink',
-              'medium sea green', 'khaki']
- 
-weights = [1, 2, 1, 1, 3, 2, 1, 4, 1, 3,
-           1, 1, 1, 4, 1, 3, 2, 2, 1, 2,
-           1, 2, 1, 2, 1]
- 
  
 class Brick(Turtle):
-    def __init__(self, x_cor, y_cor):
+    def __init__(self, x_cor, y_cor,YPAD): #note that x_cor, y_cor refer to the discrete grid ~15x10. The bottom left refers to the coordinate , the center of the paddle, 
+        # by definition is in the position (0,1)
         super().__init__()
         self.penup()
+        self.color('salmon')
         self.shape('square')
-        self.shapesize(stretch_wid=1.5, stretch_len=3)
-        self.color(random.choice(COLOR_LIST))
-        self.goto(x=x_cor, y=y_cor)
+        self.shapesize(stretch_wid=2.5, stretch_len=7.35) # not that stretch_len should be set to 7.5 because a brick has len 7.5. 7.35 is only set optically fo ensure
+        # the bricks are visually separated even if directly next to each other. This has no effect on caclculations.
+        self.goto(x=x_cor*50, y=y_cor*50+YPAD)
  
-        self.quantity = random.choice(weights)
- 
-        # Defining borders of the brick
-        self.left_wall = self.xcor() - 30
-        self.right_wall = self.xcor() + 30
-        self.upper_wall = self.ycor() + 15
-        self.bottom_wall = self.ycor() - 15
- 
- 
-class Bricks:
-    def __init__(self):
-        self.y_start = 0
-        self.y_end = 240
-        self.bricks = []
-        self.create_all_lanes()
- 
-    def create_lane(self, y_cor):
-        for i in range(-570, 570, 63):
-            brick = Brick(i, y_cor)
-            self.bricks.append(brick)
- 
-    def create_all_lanes(self):
-        for i in range(self.y_start, self.y_end, 32):
-            self.create_lane(i)
+
--- main.py
+++ main.py
@@ -1,23 +1,55 @@
 import turtle as tr
 from paddle import Paddle
 from ball import Ball
-from bricks import Bricks
+from bricks import Brick
+import math
 import time
  
 # Have a grid of HGrid x VGrid 
-HGrid = 20 # dont set below 5 otherwise the paddle is too large!
+HGrid = 15 # dont set below 5 otherwise the paddle is too large! it should also be an uneven number
 VGrid = 10
 YPAD = -225 # set the permanent Y position of the paddle
+
+#define the position of the bricks. Recall that 1 Brick extends 3 x 1, make sure they dont overlap
+Coordinates = [[0,9],[3,9],[-3,9]]
 
 screen = tr.Screen()
 screen.setup(width=780, height=600) # this should not be changed, to display the game correctly
 screen.bgcolor('black')
 screen.title('Breakout')
 screen.tracer(0)
- 
+
+#This adds a frame to the game. This is soley for asthetics and has no real effect
+LEFT = tr.Turtle()
+LEFT.penup()
+LEFT.color('lightblue')
+LEFT.shape('square')
+LEFT.goto(-50*math.ceil(HGrid/2), YPAD+50*VGrid/2)
+LEFT.shapesize(stretch_wid=2.5*(1+VGrid), stretch_len=2.5)
+RIGHT = tr.Turtle()
+RIGHT.penup()
+RIGHT.color('lightblue')
+RIGHT.shape('square')
+RIGHT.goto(50*math.ceil(HGrid/2), YPAD+50*VGrid/2)
+RIGHT.shapesize(stretch_wid=2.5*(1+VGrid), stretch_len=2.5)
+TOP = tr.Turtle()
+TOP.penup()
+TOP.color('lightblue')
+TOP.shape('square')
+TOP.goto(0, YPAD+50*(1+VGrid))
+TOP.shapesize(stretch_wid=2.5, stretch_len=2.5*(2+HGrid))
+########################### End of Fram definiton
+
+
 paddle = Paddle(YPAD)
 ball = Ball(YPAD)
- 
+
+# set the coordinate for Bricks
+
+Bricks = []
+for i in Coordinates:
+    Bricks.append(Brick(i[0],i[1],YPAD))
+
 playing_game = True
  
  
